fbb5433f3c89fcab85998c10ff2e516a091bcd8a
leleqin <602568224@qq.com>
2018-04-12 22:07:23 +0800

    collection完成

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,17 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  collection_b.map(
+        item =>
+    {
+        if (!collection_a.includes(item)){
+          collection_a.push(item);
+        }
+    }
+  );
+
+  return collection_a;
+
 }
 
 module.exports = get_union;
