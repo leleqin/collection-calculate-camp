2a4660afa10bb44982ea138f66174592d16670d8
leleqin <602568224@qq.com>
2018-04-12 21:10:08 +0800

    collection_7

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,20 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  let n = ["","a","b","c","d","e"];
+  let m = [];
+  if (number_a < number_b){
+    for (let i = number_a;i <= number_b;i++){
+      m.push(n[i]);
+    }
+  }else if (number_a > number_b){
+    for (let i = number_a;i >= number_b;i--){
+      m.push(n[i]);
+    }
+  }else if (number_a == number_b){
+    m.push(n[number_a]);
+  }
+  return m;
 }
 
 module.exports = get_letter_interval;
