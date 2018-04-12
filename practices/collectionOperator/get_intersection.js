fbd19ceedcd56cb91b7629f10afc947b260f627e
leleqin <602568224@qq.com>
2018-04-12 17:19:37 +0800

    collection_5

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -3,10 +3,10 @@
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
   let num = [];
-  for(let i = 0;i < collection_a.length;i++){
-    for(let j = 0;j < collection_b.length;j++){
-      if(collection_a[i] == collection_b[j]){
-        num.push(collection_a);
+  for(let i = 0;i < collection_b.length;i++){
+    for(let j = 0;j < collection_a.length;j++){
+      if(collection_a[j] == collection_b[i]){
+        num.push(collection_b[i]);
       }
     }
   }
