2edbb662f3387fe2110ba0ebaeeae7a0041735fb
leleqin <602568224@qq.com>
2018-04-13 15:35:23 +0800

    filter_3

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -3,6 +3,10 @@
 function choose_no_repeat_number(collection) {
 
   //在这里写入代码
+  let n = collection.filter(function (element,index,self) {
+    return self.indexOf(element) == index;
+  })
+  return n;
 }
 
 module.exports = choose_no_repeat_number;
