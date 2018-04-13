8cf9205f472535c17f9ce03cdd7743083a6457a9
leleqin <602568224@qq.com>
2018-04-13 15:28:16 +0800

    filter_2

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,6 +3,10 @@
 function choose_multiples_of_three(collection) {
 
   //在这里写入代码
+  let n = collection.filter(function (x) {
+    return x % 3 == 0;
+  })
+  return n;
 }
 
 module.exports = choose_multiples_of_three;
