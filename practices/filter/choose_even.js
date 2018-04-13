e0ad52684f874a920eb6f2fe58af5cc4b74bd52b
leleqin <602568224@qq.com>
2018-04-13 15:25:30 +0800

    filter_1

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,6 +3,20 @@
 function choose_even(collection) {
 
   //在这里写入代码
+  //原始方法
+  /*let n = [];
+  for (let i = 0;i < collection.length;i++ ){
+    if (collection[i] % 2 == 0){
+      n.push(collection[i]);
+    }
+  }
+return n;*/
+
+  //方法二
+  let n = collection.filter(function (x) {
+    return x % 2 == 0;
+  })
+  return n;
 }
 
 module.exports = choose_even;
