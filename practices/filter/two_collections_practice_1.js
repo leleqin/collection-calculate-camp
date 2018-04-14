b3bc428b55a66da2cc837a2abb4da8f4e2f2ce31
leleqin <602568224@qq.com>
2018-04-14 10:16:15 +0800

    filter_5

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -3,7 +3,7 @@
 function choose_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
-
+  return collection_a.filter(element => collection_b.includes(element));
 }
 
 module.exports = choose_common_elements;
