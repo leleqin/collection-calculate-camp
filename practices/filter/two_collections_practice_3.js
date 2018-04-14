450b24b95174627c28031e7fbccf1a99306778be
leleqin <602568224@qq.com>
2018-04-14 10:41:51 +0800

    filter完成

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,11 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  return collection_a.filter(element => divisible(collection_b, element))
 }
+function divisible(collection, element) {
+  return collection.some(t => element % t === 0);
+  }
+
 
 module.exports = choose_divisible_integer;
