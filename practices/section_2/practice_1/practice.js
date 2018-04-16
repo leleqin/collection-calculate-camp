8f6ae41fe9436198f38495b2469ac8ea473b8cf4
leleqin <602568224@qq.com>
2018-04-16 21:29:36 +0800

    sup_collection_1

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,14 +1,5 @@
 function count_same_elements(collection) {
   //在这里写入代码
-  let m = new Map();
-  let count;
-  for (let i = 0;i < collection.length;i++){
-    if (collection[i]=="a"){
-      count++;
-      m.set("a",count);
-    }
-  }
-  console.log("key:"+m.get("a")+"count:"+count);
 }
 
 module.exports = count_same_elements;
--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -3,6 +3,13 @@
 function even_to_letter(collection) {
 
   //在这里写入代码
+  let n = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let m = [];
+  collection.map(element => {
+    if (element % 2 == 0)
+      m.push(n[element]);
+  });
+  return m;
 }
 
 module.exports = even_to_letter;
