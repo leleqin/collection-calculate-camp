c56391b2a71363fd93a77f1deb141fdfee4584c4
leleqin <602568224@qq.com>
2018-04-16 19:35:09 +0800

    reduce_5

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,6 +2,15 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
-}
+  let n = collection.split("->");
+  //注意sort排序
+  n.sort((a, b) => a - b > 0 ? 1 : -1);
+  if (n.length % 2 == 0)
+    //注意字符串转数字！
+   return (parseInt(n[n.length / 2 -1] )+ parseInt(n[n.length / 2])) / 2;
+  else
+   return  n[(Math.ceil(n.length/2))-1];
+
+  }
 
 module.exports = compute_chain_median;
