85ea02940f7701e675e3e54ed4f71e4174737e69
leleqin <602568224@qq.com>
2018-04-16 20:07:57 +0800

    reduce_6

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -2,6 +2,12 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  let n = collection.sort((a,b) => a - b > 0 ? 1:-1);
+
+  if (n.length % 2 !== 0)
+    return n[parseInt(n.length / 2)];
+   else
+     return (parseInt(n[n.length / 2 - 1]) + parseInt(n[n.length / 2])) / 2;
 }
 
 module.exports = compute_median;
