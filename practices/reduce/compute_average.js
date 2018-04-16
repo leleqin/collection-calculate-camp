1c0dda66cb4022675cf08241e6e8e51669d03245
leleqin <602568224@qq.com>
2018-04-16 18:52:55 +0800

    reduce_4

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -2,6 +2,9 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  let n = collection.reduce((x,y) => x+y);
+  let m = n/collection.length;
+  return m;
 }
 
 module.exports = compute_average;
