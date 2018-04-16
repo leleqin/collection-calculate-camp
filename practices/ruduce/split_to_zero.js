67b343f1c1497effcc3b06e504af738aab08bc8d
leleqin <602568224@qq.com>
2018-04-16 20:59:13 +0800

    ruduce完成

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -2,6 +2,22 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  let n = [];
+  let index = number * 10;
+  interval *= 10;
+
+  n.push(number);
+  while(index > interval){
+    n.push((index-interval)/10);
+    index -= interval;
+  }
+
+  if(number % interval == 0)
+    return n;
+
+  n.push((index - interval)/10);
+
+  return n;
 
 }
 
