44fe2fe2b7a7fe981e512e55c90de2b25bbe0bf5
leleqin <602568224@qq.com>
2018-04-16 10:21:57 +0800

    flaten完成

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function double_to_one(collection) {
-
+  let l = [].concat.apply([],collection);
+  return l.filter(function (element, index, self) {
+    return self.indexOf(element) === index;
+  });
   //在这里写入代码
 }
 
