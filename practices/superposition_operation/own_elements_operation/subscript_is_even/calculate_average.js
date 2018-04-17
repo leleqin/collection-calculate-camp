db9ae0ff9f06cc844dfbe47ad8785b8c858491ad
leleqin <602568224@qq.com>
2018-04-17 15:18:00 +0800

    super_subscript_2

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,8 @@
 'use strict';
 var calculate_average = function(collection){
 
+  let n = collection.filter(element => element % 2 == 0);
+  let m = n.reduce((x,y) => x+y);
+  return m/n.length;
 };
 module.exports = calculate_average;
