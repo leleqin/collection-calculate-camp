4c6d4003b807bf0237a58b6bf2076035238fda2f
leleqin <602568224@qq.com>
2018-04-18 12:10:47 +0800

    section_2_2

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,13 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  let result = [];
+
+  collection.forEach(item => {
+    if (!result.find(element => element.key === item))
+      result.push({key: item, count: collection.filter(element => element === item).length});
+  });
+
+  return result;
 }
 
 module.exports = count_same_elements;
