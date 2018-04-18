853538b530a6432b882201a7a3cfd720141fad44
leleqin <602568224@qq.com>
2018-04-18 11:34:13 +0800

    super_own_subscript完成

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,9 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
-
+  let n = collection.filter(element => element % 2 === 0);
+  let n2 = n.sort((a,b) => a - b > 0?1:-1);
+  let m = collection.filter(element => element % 2 != 0);
+  let m2 = m.sort((a,b) => a - b > 0?1:-1).reverse();
+  return n2.concat(m2);
 };
 module.exports = even_asc_odd_desc;
--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,18 @@
 'use strict';
 var single_element = function(collection){
+  let result = [];
+  let isExist = true;
 
+  for (let outerIndex = 1; outerIndex < collection.length; outerIndex += 2) {
+    for (let innerIndex = 1; innerIndex < collection.length; innerIndex += 2) {
+      if (collection[outerIndex] === collection[innerIndex] && outerIndex !== innerIndex) {
+        isExist = false;
+      }
+    }
+    if (isExist) result.push(collection[outerIndex]);
+    isExist = true;
+  }
+
+  return result;
 };
 module.exports = single_element;
