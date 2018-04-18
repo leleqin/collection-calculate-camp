2b9f1de2029bbdc59d8d9338c15a520c2afb1239
leleqin <602568224@qq.com>
2018-04-18 12:49:57 +0800

    super_sub完成

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,6 @@
 'use strict';
 var is_exist_element = function(collection,element){
 
+return collection.indexOf(element) % 2 == 0?true:false;
 };
 module.exports = is_exist_element;
--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,14 @@
 'use strict';
 var single_element = function(collection){
 
-};
+  collection.unshift("");
+  let n = [];
+  for (let i = 0;i < collection.length;i++){
+    n.push(collection[i % 2 === 0]);
+  }
+
+  return n.filter(element => m(n,element));
+
+
+}
 module.exports = single_element;
